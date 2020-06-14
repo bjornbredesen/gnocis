c85b9351d2e8924eaf2a17640edcb99b85c573bb
Bjørn Bredesen <bjorn@bjornbredesen.no>
2020-06-14 23:11:31 +0200

    Added Keras sequence model

--- gnocis/tfModels.py
+++ gnocis/tfModels.py
@@ -309,3 +309,115 @@ class sequenceModelMultiCNN(sequenceModel):
 
 	def __repr__(self): return self.__str__()
 
+
+# Convolutional Neural Network
+class sequenceModelKeras(sequenceModel):
+	def __init__(self, name, windowSize, windowStep, kerasModel, epochs = 100, targetLabel = positive, labels = [ positive, negative ], trainingSet = None, batchsize = 1000):
+		super().__init__(name, enableMultiprocessing = False)
+		self.windowSize, self.windowStep = windowSize, windowStep
+		self.targetLabel, self.labels = targetLabel, list(set([targetLabel]) | set(labels))
+		self.nLabels = len(self.labels)
+		self.threshold = 0.0
+		self.kerasModel = kerasModel
+		self.epochs = epochs
+		self.cls = None
+		self.batchsize = batchsize
+		if trainingSet is not None:
+			self.train(trainingSet)
+	
+	def train(self, trainingSet):
+		print('Training')
+		self.trainingSet = trainingSet
+		scaleFac = 10
+		bloat = int((min(500, self.windowSize)-1)/scaleFac)
+		hbloat = int(bloat/2)
+		
+		model = self.kerasModel
+		
+		model.compile(
+			optimizer = 'adam',
+			loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+			metrics = ['accuracy']
+		)
+		
+		self.labelValues = {
+			label: i
+			for i, label in enumerate(self.labels)
+		}
+		train_seq = np.array([
+			getSequenceMatrix(win.seq).reshape(len(win), 4, 1)
+			for label in self.labels
+			for seq in trainingSet.withLabel(label)
+			for win in seq.windows(self.windowSize, self.windowStep)
+		])
+		train_labels = np.array([
+			self.labelValues[label]
+			for label in self.labels
+			for seq in trainingSet.withLabel(label)
+			for win in seq.windows(self.windowSize, self.windowStep)
+		])
+
+		model.fit(
+			train_seq,
+			train_labels,
+			epochs = self.epochs,
+			verbose = 0
+		)
+		
+		self.cls = model
+		print(' ... done!')
+	
+	def getSequenceScores(self, seqs):
+		if self.batchsize == 0:
+			return super().getSequenceScores(seqs)
+		if isinstance(seqs, sequenceStream):
+			nTreadFetch = 100000
+			maxThreadFetchNT = nTreadFetch * 1000
+			seqwinit = (
+				(i, win)
+				for blk in seqs.fetch(nTreadFetch, maxThreadFetchNT)
+				for i, cseq in enumerate(blk)
+				for win in cseq.windows(self.windowSize, self.windowStep)
+				if len(win) == self.windowSize
+			)
+		else:
+			seqwinit = (
+				(i, win)
+				for i, cseq in enumerate(seqs)
+				for win in cseq.windows(self.windowSize, self.windowStep)
+				if len(win) == self.windowSize
+			)
+		seqscores = [  ]
+		while True:
+			batch = [
+				b for b in [
+					next(seqwinit, None) for _ in range(self.batchsize)
+				]
+				if b is not None
+			]
+			if len(batch) == 0: break
+			p = self.cls.predict(np.array([getSequenceMatrix(seq.seq).reshape(len(seq), 4, 1) for i, seq in batch]))
+			#scores = p[:, 1] - p[:, 0]
+			i = self.labelValues[self.targetLabel]
+			scores = p[:, i] / sum(p[:, n] for n in range(self.nLabels))
+			for score, (i, win) in zip(scores, batch):
+				if i >= len(seqscores):
+					seqscores += [ -float('INF') for _ in range(i - len(seqscores) + 1) ]
+				if score > seqscores[i]:
+					seqscores[i] = score
+		#
+		return seqscores
+	
+	def scoreWindow(self, seq):
+		p = self.cls.predict(np.array([getSequenceMatrix(seq.seq).reshape(len(seq), 4, 1)]))
+		i = self.labelValues[self.targetLabel]
+		return p[0, i] / sum(p[0, n] for n in range(self.nLabels))
+	
+	def getTrainer(self):
+		return lambda ts: sequenceModelKeras(self.name, trainingSet = ts, windowSize = self.windowSize, windowStep = self.windowStep, kerasModel = self.kerasModel, epochs = self.epochs, targetLabel = self.targetLabel, labels = self.labels, batchsize = self.batchsize)
+
+	def __str__(self):
+		return 'Multi-class Keras Neural Network<Training set: %s; Positive label: %s; Negative label: %s; Epochs: %d>'%(str(self.trainingSet), str(self.labelPositive), str(self.labelNegative), self.epochs)
+
+	def __repr__(self): return self.__str__()
+
