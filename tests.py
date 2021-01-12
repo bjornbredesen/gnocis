8df7bd2b289ecb9b68feafb04c9ed757679891a8
Bjørn Bredesen <bjorn@bjornbredesen.no>
2021-01-12 08:40:24 +0100

    Added test of sequence stream scoring

--- tests.py
+++ tests.py
@@ -11,6 +11,7 @@ from os import mkdir, rmdir
 
 import unittest
 
+
 #-----------------------------------
 # Data
 
@@ -29,7 +30,10 @@ if __name__ == '__main__':
 		rmdir('temp')
 	except:
 		pass
-	mkdir('temp')
+	try:
+		mkdir('temp')
+	except:
+		pass
 	genome = nc.streamFASTAGZ('tutorial/DmelR5.fasta.gz',
 			restrictToSequences = [ '2L', '2R', '3L', '3R', '4', 'X' ])
 	#
@@ -70,6 +74,7 @@ if __name__ == '__main__':
 		 	for _ in range(1000) )),
 	 ])
 	print('    ... done!')
+
 
 #-----------------------------------
 # Regions
@@ -193,45 +198,9 @@ class testSequences(unittest.TestCase):
 
 
 #-----------------------------------
-# Modelling
+# Features
 
-# Test
-
-class testModels(unittest.TestCase):
-	
-	def testSequenceWindows(self):
-		tpos = nc.sequences('PcG/TrxG 1', PRESeq[:int(len(PRESeq)/2)]).label(nc.positive)
-		tneg = MC.generateSet(n = int(len(PRESeq)/2), length = 3000).label(nc.negative)
-		testset = tpos
-		nSpectrum = 5
-		winSize = 500
-		winStep = 250
-		kDegree = 2
-		featureSet = nc.features.kSpectrumMM(nSpectrum)
-		mdl = sklnc.sequenceModelSVM( name = 'SVM', features = featureSet, trainingSet = tpos + tneg, windowSize = winSize, windowStep = winStep, kDegree = kDegree )
-		# Single-core
-		nc.setNCores(1)
-		scoresA = mdl.getSequenceScores(testset)
-		# Multi-core
-		nc.setNCores(4)
-		scoresB = mdl.getSequenceScores(testset)
-		#
-		diff = sum( 1 if a != b else 0 for a, b in zip(scoresA, scoresB) )
-		self.assertEqual(diff, 0)
-		# Train second set of models, to ensure old model cannot get stuck
-		# in one process
-		tpos = nc.sequences('PcG/TrxG 2', PRESeq[int(len(PRESeq)/2):]).label(nc.positive)
-		tneg = MC.generateSet(n = len(PRESeq)-int(len(PRESeq)/2), length = 3000).label(nc.negative)
-		featureSet = nc.features.kSpectrumMM(nSpectrum)
-		mdl2 = sklnc.sequenceModelSVM( name = 'SVM', features = featureSet, trainingSet = tpos + tneg, windowSize = winSize, windowStep = winStep, kDegree = kDegree )
-		#
-		scoresD = mdl2.getSequenceScores(testset)
-		# Single-core
-		nc.setNCores(1)
-		scoresC = mdl2.getSequenceScores(testset)
-		#
-		diff = sum( 1 if a != b else 0 for a, b in zip(scoresC, scoresD) )
-		self.assertEqual(diff, 0)
+class testFeatures(unittest.TestCase):
 	
 	def testKSpectrum(self):
 		spec = nc.features.kSpectrum(5)
@@ -330,6 +299,73 @@ class testModels(unittest.TestCase):
 			if f > 0
 		)
 		self.assertEqual(specmotifs, motifs)
+
+
+#-----------------------------------
+# Modelling
+
+class testModels(unittest.TestCase):
+	
+	def testSequenceWindows(self):
+		tpos = nc.sequences('PcG/TrxG 1', PRESeq[:int(len(PRESeq)/2)]).label(nc.positive)
+		tneg = MC.generateSet(n = int(len(PRESeq)/2), length = 3000).label(nc.negative)
+		testset = tpos
+		nSpectrum = 5
+		winSize = 500
+		winStep = 250
+		kDegree = 2
+		featureSet = nc.features.kSpectrumMM(nSpectrum)
+		mdl = sklnc.sequenceModelSVM( name = 'SVM', features = featureSet, trainingSet = tpos + tneg, windowSize = winSize, windowStep = winStep, kDegree = kDegree )
+		# Single-core
+		nc.setNCores(1)
+		scoresA = mdl.getSequenceScores(testset)
+		# Multi-core
+		nc.setNCores(4)
+		scoresB = mdl.getSequenceScores(testset)
+		#
+		diff = sum( 1 if a != b else 0 for a, b in zip(scoresA, scoresB) )
+		self.assertEqual(diff, 0)
+		# Train second set of models, to ensure old model cannot get stuck
+		# in one process
+		tpos = nc.sequences('PcG/TrxG 2', PRESeq[int(len(PRESeq)/2):]).label(nc.positive)
+		tneg = MC.generateSet(n = len(PRESeq)-int(len(PRESeq)/2), length = 3000).label(nc.negative)
+		featureSet = nc.features.kSpectrumMM(nSpectrum)
+		mdl2 = sklnc.sequenceModelSVM( name = 'SVM', features = featureSet, trainingSet = tpos + tneg, windowSize = winSize, windowStep = winStep, kDegree = kDegree )
+		#
+		scoresD = mdl2.getSequenceScores(testset)
+		# Single-core
+		nc.setNCores(1)
+		scoresC = mdl2.getSequenceScores(testset)
+		#
+		diff = sum( 1 if a != b else 0 for a, b in zip(scoresC, scoresD) )
+		self.assertEqual(diff, 0)
+	
+	def testSequenceScoringWithStream(self):
+		Kahn2014Rgn = nc.loadGFF('tutorial/Kahn2014.GFF')
+		Kahn2014Seq = Kahn2014Rgn \
+				       .recenter(3000) \
+				       .extract(genome)
+		tneg = MC.generateSet(n = len(Kahn2014Seq), length = 3000)
+		trainingSet = Kahn2014Seq.label(nc.positive) + tneg.label(nc.negative)
+		PyPREdictor = nc.motifs.Ringrose2003GTGT() \
+				       .pairFreq(distCut = 219) \
+				       .model(nc.logOdds(
+				              labelPositive = nc.positive,
+				              labelNegative = nc.negative)
+				       ) \
+				       .sequenceModel(name = 'PyPREdictor (M2003+GTGT)',
+				                      windowSize = 500, windowStep = 250) \
+				       .train(trainingSet)
+		PyPREdictor.batchsize = 100
+		MC.generateSet(n = 1000, length = 3000).saveFASTA('./temp/test.Background.fasta')
+		seq1 = nc.streamFASTA('./temp/test.Background.fasta')
+		seq2 = nc.loadFASTA('./temp/test.Background.fasta')
+		score1 = PyPREdictor.getSequenceScores(seq1, nStreamFetch = 1000)
+		score2 = PyPREdictor.getSequenceScores(seq2, nStreamFetch = 1000)
+		diff = sum(abs(b - a) for a, b in zip(score1, score2))
+		assert(diff == 0.0)
+		assert(len(score1) == len(score2))
+
 
 #-----------------------------------
 
