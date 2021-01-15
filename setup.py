a38163daf86f2c94e3805aa46f628c921fd9163f
Bjørn Bredesen <bjorn@bjornbredesen.no>
2021-01-15 20:58:08 +0100

    Added building of binary wheels for all relevant Python versions

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
 	packages = [ "gnocis" ],
 	author = "Bjørn Bredesen",
 	author_email = "bjorn@bjornbredesen.no",
-	version = "0.9.8",
+	version = "0.9.9a1",
 	url = "https://github.com/bjornbredesen/gnocis",
 	license = "MIT",
 	description = "Gnocis is a system for the analysis and the modelling of cis-regulatory DNA sequences.",
