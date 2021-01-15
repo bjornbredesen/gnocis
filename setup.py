62be62418035c1030dcc2a2e30371cdc35c30c1c
Bjørn Bredesen <bjorn@bjornbredesen.no>
2021-01-15 19:07:02 +0100

    Release 0.9.8

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
 	packages = [ "gnocis" ],
 	author = "Bjørn Bredesen",
 	author_email = "bjorn@bjornbredesen.no",
-	version = "0.9.8a1",
+	version = "0.9.8",
 	url = "https://github.com/bjornbredesen/gnocis",
 	license = "MIT",
 	description = "Gnocis is a system for the analysis and the modelling of cis-regulatory DNA sequences.",
